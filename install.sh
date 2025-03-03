3b1e95c6301a2d01cac42aea148bce4df5d2993b
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2025-03-03 09:56:10 -0300

    refactor(install.sh): Shellfmt go BRRRRRRRRRRT

--- install.sh
+++ install.sh
@@ -40,60 +40,60 @@ else
 
     # ---->>>> Verificação do sistema
     case $OS_NAME in
-        ubuntu)
-            case $VERSION in
-                24.*|22.*|20.*|18.*)
-                    show_progress "Sistema Ubuntu suportado, continuando..."
-                    ;;
-                *)
-                    error_exit "Versão do Ubuntu não suportada. Use 18, 20, 22 ou 24."
-                    ;;
-            esac
-            ;;
-        debian)
-            case $VERSION in
-                12*|11*|10*|9*)
-                    show_progress "Sistema Debian suportado, continuando..."
-                    ;;
-                *)
-                    error_exit "Versão do Debian não suportada. Use 9, 10, 11 ou 12."
-                    ;;
-            esac
-            ;;
-        almalinux|rocky)
-            case $VERSION in
-                9*|8*)
-                    show_progress "Sistema $OS_NAME suportado, continuando..."
-                    ;;
-                *)
-                    error_exit "Versão do $OS_NAME não suportada. Use 8 ou 9."
-                    ;;
-            esac
+    ubuntu)
+        case $VERSION in
+        24.* | 22.* | 20.* | 18.*)
+            show_progress "Sistema Ubuntu suportado, continuando..."
             ;;
         *)
-            error_exit "Sistema não suportado. Use Ubuntu, Debian, AlmaLinux ou Rocky Linux."
+            error_exit "Versão do Ubuntu não suportada. Use 18, 20, 22 ou 24."
             ;;
+        esac
+        ;;
+    debian)
+        case $VERSION in
+        12* | 11* | 10* | 9*)
+            show_progress "Sistema Debian suportado, continuando..."
+            ;;
+        *)
+            error_exit "Versão do Debian não suportada. Use 9, 10, 11 ou 12."
+            ;;
+        esac
+        ;;
+    almalinux | rocky)
+        case $VERSION in
+        9* | 8*)
+            show_progress "Sistema $OS_NAME suportado, continuando..."
+            ;;
+        *)
+            error_exit "Versão do $OS_NAME não suportada. Use 8 ou 9."
+            ;;
+        esac
+        ;;
+    *)
+        error_exit "Sistema não suportado. Use Ubuntu, Debian, AlmaLinux ou Rocky Linux."
+        ;;
     esac
     increment_step
 
     # ---->>>> Instalação de pacotes requisitos e atualização do sistema
     show_progress "Atualizando o sistema..."
     case $OS_NAME in
-        ubuntu|debian)
-            apt-get update -y > /dev/null 2>&1 || error_exit "Falha ao atualizar o sistema"
-            apt-get install gnupg curl build-essential git cmake clang sysstat net-tools sqlite3 libsqlite3-dev zip tar iptables ca-certificates -y > /dev/null 2>&1 || error_exit "Falha ao instalar pacotes"
-            ;;
-        almalinux|rocky)
-            dnf update -y > /dev/null 2>&1 || error_exit "Falha ao atualizar o sistema"
-            dnf install epel-release gnupg2 curl gcc g++ make git cmake clang sysstat net-tools sqlite sqlite-devel zip tar iptables ca-certificates -y > /dev/null 2>&1 || error_exit "Falha ao instalar pacotes"
-            ;;
+    ubuntu | debian)
+        apt-get update -y >/dev/null 2>&1 || error_exit "Falha ao atualizar o sistema"
+        apt-get install gnupg curl build-essential git cmake clang sysstat net-tools sqlite3 libsqlite3-dev zip tar iptables ca-certificates -y >/dev/null 2>&1 || error_exit "Falha ao instalar pacotes"
+        ;;
+    almalinux | rocky)
+        dnf update -y >/dev/null 2>&1 || error_exit "Falha ao atualizar o sistema"
+        dnf install epel-release gnupg2 curl gcc g++ make git cmake clang sysstat net-tools sqlite sqlite-devel zip tar iptables ca-certificates -y >/dev/null 2>&1 || error_exit "Falha ao instalar pacotes"
+        ;;
     esac
     increment_step
 
     # ---->>>> Criando o diretorio do script
     show_progress "Criando diretorio /opt/rustymanager..."
-    mkdir /opt/ > /dev/null 2>&1
-    mkdir /opt/rustymanager > /dev/null 2>&1
+    mkdir /opt/ >/dev/null 2>&1
+    mkdir /opt/rustymanager >/dev/null 2>&1
     increment_step
 
     # ---->>>> Criando as colunas no banco de dados
@@ -125,8 +125,8 @@ else
 
     # ---->>>> Instalar rust
     show_progress "Instalando Rust..."
-    if ! command -v rustc &> /dev/null; then
-        curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y > /dev/null 2>&1 || error_exit "Falha ao instalar Rust"
+    if ! command -v rustc &>/dev/null; then
+        curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y >/dev/null 2>&1 || error_exit "Falha ao instalar Rust"
         . "$HOME/.cargo/env"
     fi
     increment_step
@@ -135,20 +135,20 @@ else
     show_progress "Compilando RustyManager, isso pode levar bastante tempo dependendo da maquina..."
     mkdir -p /opt/rustymanager
     mkdir -p /opt/rustymanager/ssl
-    git clone --branch "$SCRIPT_VERSION" --recurse-submodules --single-branch https://github.com/UlekBR/RustyManager.git /root/RustyManager > /dev/null 2>&1 || error_exit "Falha ao clonar RustyManager"
+    git clone --branch "$SCRIPT_VERSION" --recurse-submodules --single-branch https://github.com/UlekBR/RustyManager.git /root/RustyManager >/dev/null 2>&1 || error_exit "Falha ao clonar RustyManager"
 
     cd /root/RustyManager/
-    mv -f ./Utils/ssl/cert.pem /opt/rustymanager/ssl/cert.pem > /dev/null 2>&1
-    mv -f ./Utils/ssl/key.pem /opt/rustymanager/ssl/key.pem > /dev/null 2>&1
+    mv -f ./Utils/ssl/cert.pem /opt/rustymanager/ssl/cert.pem >/dev/null 2>&1
+    mv -f ./Utils/ssl/key.pem /opt/rustymanager/ssl/key.pem >/dev/null 2>&1
 
     ## definindo compilador
     export CC=clang
-    cargo build --release --jobs $(nproc) > /dev/null 2>&1 || error_exit "Falha ao compilar RustyManager"
-    mv -f ./target/release/SshScript /opt/rustymanager/manager > /dev/null 2>&1
-    mv -f ./target/release/CheckUser /opt/rustymanager/checkuser > /dev/null 2>&1
-    mv -f ./target/release/RustyProxy /opt/rustymanager/rustyproxy > /dev/null 2>&1
-    mv -f ./target/release/RustyProxySSL /opt/rustymanager/rustyproxyssl > /dev/null 2>&1
-    mv -f ./target/release/ConnectionsManager /opt/rustymanager/connectionsmanager > /dev/null 2>&1
+    cargo build --release --jobs $(nproc) >/dev/null 2>&1 || error_exit "Falha ao compilar RustyManager"
+    mv -f ./target/release/SshScript /opt/rustymanager/manager >/dev/null 2>&1
+    mv -f ./target/release/CheckUser /opt/rustymanager/checkuser >/dev/null 2>&1
+    mv -f ./target/release/RustyProxy /opt/rustymanager/rustyproxy >/dev/null 2>&1
+    mv -f ./target/release/RustyProxySSL /opt/rustymanager/rustyproxyssl >/dev/null 2>&1
+    mv -f ./target/release/ConnectionsManager /opt/rustymanager/connectionsmanager >/dev/null 2>&1
 
     increment_step
 
@@ -156,8 +156,8 @@ else
     show_progress "Compilando BadVPN..."
     mkdir -p /root/RustyManager/BadVpn/src/badvpn-build
     cd /root/RustyManager/BadVpn/src/badvpn-build
-    cmake .. -DBUILD_NOTHING_BY_DEFAULT=1 -DBUILD_UDPGW=1 > /dev/null 2>&1 || error_exit "Falha ao configurar cmake para BadVPN"
-    make > /dev/null 2>&1 || error_exit "Falha ao compilar BadVPN"
+    cmake .. -DBUILD_NOTHING_BY_DEFAULT=1 -DBUILD_UDPGW=1 >/dev/null 2>&1 || error_exit "Falha ao configurar cmake para BadVPN"
+    make >/dev/null 2>&1 || error_exit "Falha ao compilar BadVPN"
     mv -f udpgw/badvpn-udpgw /opt/rustymanager/badvpn
     increment_step
 
@@ -170,62 +170,60 @@ else
     ln -sf /opt/rustymanager/manager /usr/local/bin/menu
     increment_step
 
-
     # ---->>>> Instalar speedtest
     show_progress "Instalando Speedtest..."
     case $OS_NAME in
-        ubuntu|debian)
-            curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | bash > /dev/null 2>&1 || error_exit "Falha ao baixar e instalar o script do speedtest"
-            apt-get install speedtest -y > /dev/null 2>&1 || error_exit "Falha ao instalar o speedtest"
-            ;;
-        almalinux|rocky)
-            curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.rpm.sh | bash > /dev/null 2>&1 || error_exit "Falha ao baixar e instalar o script do speedtest"
-            dnf install speedtest -y > /dev/null 2>&1 || error_exit "Falha ao instalar o speedtest"
-            ;;
+    ubuntu | debian)
+        curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | bash >/dev/null 2>&1 || error_exit "Falha ao baixar e instalar o script do speedtest"
+        apt-get install speedtest -y >/dev/null 2>&1 || error_exit "Falha ao instalar o speedtest"
+        ;;
+    almalinux | rocky)
+        curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.rpm.sh | bash >/dev/null 2>&1 || error_exit "Falha ao baixar e instalar o script do speedtest"
+        dnf install speedtest -y >/dev/null 2>&1 || error_exit "Falha ao instalar o speedtest"
+        ;;
     esac
     increment_step
-    
+
     # ---->>>> Instalar Htop
     show_progress "Instalando monitor de recursos..."
     case $OS_NAME in
-        ubuntu|debian)
-            apt-get install htop -y > /dev/null 2>&1 || error_exit "Falha ao instalar o htop"
-            ;;
-        almalinux|rocky)
-            dnf install htop -y > /dev/null 2>&1 || error_exit "Falha ao instalar o htop"
-            ;;
+    ubuntu | debian)
+        apt-get install htop -y >/dev/null 2>&1 || error_exit "Falha ao instalar o htop"
+        ;;
+    almalinux | rocky)
+        dnf install htop -y >/dev/null 2>&1 || error_exit "Falha ao instalar o htop"
+        ;;
     esac
     increment_step
 
     # ---->>>> Instalando OpenVPN
     show_progress "Instalando OpenVPN..."
     case $OS_NAME in
-        ubuntu|debian)
-            apt-get install -y openvpn openssl -y > /dev/null 2>&1 || error_exit "Falha ao instalar o openvpn"
-            ;;
-        almalinux|rocky)
-            dnf install -y openvpn openssl > /dev/null 2>&1 || error_exit "Falha ao instalar o openvpn"
-            ;;
+    ubuntu | debian)
+        apt-get install -y openvpn openssl -y >/dev/null 2>&1 || error_exit "Falha ao instalar o openvpn"
+        ;;
+    almalinux | rocky)
+        dnf install -y openvpn openssl >/dev/null 2>&1 || error_exit "Falha ao instalar o openvpn"
+        ;;
     esac
     if [ ! -d "/etc/openvpn/easy-rsa" ]; then
-        curl -L -o /root/EasyRSA-3.2.1.tgz "https://github.com/OpenVPN/easy-rsa/releases/download/v3.2.1/EasyRSA-3.2.1.tgz" > /dev/null 2>&1 || error_exit "Falha ao baixar EasyRSA"
-        tar xzf /root/EasyRSA-3.2.1.tgz -C /root/ > /dev/null 2>&1 || error_exit "Falha ao extrair o EasyRSA"
-        mv -f /root/EasyRSA-3.2.1/ /etc/openvpn/easy-rsa/ > /dev/null 2>&1 || error_exit "Falha ao mover o EasyRSA"
-        chown -R root:root /etc/openvpn/easy-rsa/ > /dev/null 2>&1 || error_exit "Falha ao configurar permissões do EasyRSA"
-        rm -f /root/EasyRSA-3.2.1.tgz > /dev/null 2>&1 || error_exit "Falha ao remover o arquivo do EasyRSA"
-        cd /etc/openvpn/easy-rsa/ > /dev/null 2>&1 || error_exit "Falha ao acessar pasta do EasyRSA"
-        ./easyrsa --batch init-pki > /dev/null 2>&1 || error_exit "Falha ao iniciar pki"
-        ./easyrsa --batch build-ca nopass > /dev/null 2>&1 || error_exit "Falha ao gerar certificado"
-        ./easyrsa --batch gen-dh > /dev/null 2>&1 || error_exit "Falha ao gerar dh"
-        ./easyrsa --batch build-server-full server nopass > /dev/null 2>&1 || error_exit "Falha ao gerar certificado do servidor"
-        ./easyrsa --batch build-client-full client nopass > /dev/null 2>&1 || error_exit "Falha ao gerar certificado do cliente"
-        cp pki/ca.crt pki/private/ca.key pki/dh.pem pki/issued/server.crt pki/private/server.key /etc/openvpn  > /dev/null 2>&1 || error_exit "Falha ao copiar arquivos do openvpn"
-        openvpn --genkey --secret /etc/openvpn/ta.key  > /dev/null 2>&1 || error_exit "Falha ao gerar a chave"
+        curl -L -o /root/EasyRSA-3.2.1.tgz "https://github.com/OpenVPN/easy-rsa/releases/download/v3.2.1/EasyRSA-3.2.1.tgz" >/dev/null 2>&1 || error_exit "Falha ao baixar EasyRSA"
+        tar xzf /root/EasyRSA-3.2.1.tgz -C /root/ >/dev/null 2>&1 || error_exit "Falha ao extrair o EasyRSA"
+        mv -f /root/EasyRSA-3.2.1/ /etc/openvpn/easy-rsa/ >/dev/null 2>&1 || error_exit "Falha ao mover o EasyRSA"
+        chown -R root:root /etc/openvpn/easy-rsa/ >/dev/null 2>&1 || error_exit "Falha ao configurar permissões do EasyRSA"
+        rm -f /root/EasyRSA-3.2.1.tgz >/dev/null 2>&1 || error_exit "Falha ao remover o arquivo do EasyRSA"
+        cd /etc/openvpn/easy-rsa/ >/dev/null 2>&1 || error_exit "Falha ao acessar pasta do EasyRSA"
+        ./easyrsa --batch init-pki >/dev/null 2>&1 || error_exit "Falha ao iniciar pki"
+        ./easyrsa --batch build-ca nopass >/dev/null 2>&1 || error_exit "Falha ao gerar certificado"
+        ./easyrsa --batch gen-dh >/dev/null 2>&1 || error_exit "Falha ao gerar dh"
+        ./easyrsa --batch build-server-full server nopass >/dev/null 2>&1 || error_exit "Falha ao gerar certificado do servidor"
+        ./easyrsa --batch build-client-full client nopass >/dev/null 2>&1 || error_exit "Falha ao gerar certificado do cliente"
+        cp pki/ca.crt pki/private/ca.key pki/dh.pem pki/issued/server.crt pki/private/server.key /etc/openvpn >/dev/null 2>&1 || error_exit "Falha ao copiar arquivos do openvpn"
+        openvpn --genkey --secret /etc/openvpn/ta.key >/dev/null 2>&1 || error_exit "Falha ao gerar a chave"
     fi
 
-
     plugin_path=$(find /usr -type f -name 'openvpn-plugin-auth-pam.so')
- echo "port none
+    echo "port none
 proto none
 dev tun
 sndbuf 0
@@ -255,22 +253,21 @@ client-to-client
 client-cert-not-required
 username-as-common-name
 plugin $plugin_path login
-duplicate-cn" > /etc/openvpn/server.conf || error_exit "Falha ao criar openvpn server.conf"
+duplicate-cn" >/etc/openvpn/server.conf || error_exit "Falha ao criar openvpn server.conf"
 
-    echo 1 > /proc/sys/net/ipv4/ip_forward > /dev/null 2>&1 || error_exit "Falha ao habilitar ip forwarding"
-    sed -i '/net.ipv4.ip_forward/s/^#//g' /etc/sysctl.conf > /dev/null 2>&1 || error_exit "Falha ao habilitar ip forwarding"
-    sysctl -p > /dev/null 2>&1 || error_exit "Falha ao habilitar ip forwarding"
-    iptables -t nat -F > /dev/null 2>&1 || error_exit "Falha ao limpar regras do iptables"
-    iptables -F > /dev/null 2>&1 || error_exit "Falha ao limpar regras do iptables"
-    iptables -P INPUT ACCEPT > /dev/null 2>&1 || error_exit "Falha ao adicionar regras do iptables"
-    iptables -P FORWARD ACCEPT > /dev/null 2>&1 || error_exit "Falha ao adicionar regras do iptables"
-    iptables -P OUTPUT ACCEPT > /dev/null 2>&1 || error_exit "Falha ao adicionar regras do iptables"
-    iptables -t nat -A POSTROUTING -o $(ip route | grep default | awk '{print $5}') -j MASQUERADE > /dev/null 2>&1 || error_exit "Falha ao adicionar regra para permitir o trafego do openvpn no iptables"
+    echo 1 >/proc/sys/net/ipv4/ip_forward >/dev/null 2>&1 || error_exit "Falha ao habilitar ip forwarding"
+    sed -i '/net.ipv4.ip_forward/s/^#//g' /etc/sysctl.conf >/dev/null 2>&1 || error_exit "Falha ao habilitar ip forwarding"
+    sysctl -p >/dev/null 2>&1 || error_exit "Falha ao habilitar ip forwarding"
+    iptables -t nat -F >/dev/null 2>&1 || error_exit "Falha ao limpar regras do iptables"
+    iptables -F >/dev/null 2>&1 || error_exit "Falha ao limpar regras do iptables"
+    iptables -P INPUT ACCEPT >/dev/null 2>&1 || error_exit "Falha ao adicionar regras do iptables"
+    iptables -P FORWARD ACCEPT >/dev/null 2>&1 || error_exit "Falha ao adicionar regras do iptables"
+    iptables -P OUTPUT ACCEPT >/dev/null 2>&1 || error_exit "Falha ao adicionar regras do iptables"
+    iptables -t nat -A POSTROUTING -o $(ip route | grep default | awk '{print $5}') -j MASQUERADE >/dev/null 2>&1 || error_exit "Falha ao adicionar regra para permitir o trafego do openvpn no iptables"
 
-    systemctl stop openvpn@server > /dev/null 2>&1
-    systemctl disable openvpn@server > /dev/null 2>&1
+    systemctl stop openvpn@server >/dev/null 2>&1
+    systemctl disable openvpn@server >/dev/null 2>&1
     increment_step
-
 
     # ---->>>> Limpeza
     show_progress "Limpando diretórios temporários..."
